1613ba190fede7fee7683fc7365a35574fa39c2d
Paulina <wojtowiczpaulina37@gmail.com>
2022-12-11 19:39:35 +0100

    changed add-subscription component

--- src/components/dashboard/Add_subscription.jsx
+++ src/components/dashboard/Add_subscription.jsx
@@ -1,99 +1,120 @@
 import React, {useState} from "react";
-import {useEffect} from "react";
 import 'primeicons/primeicons.css';
 import Your_subscriptions from "./Your_subscriptions.jsx";
 
 
 
-const initialValues = {
-    name: "",
-    username: "",
-    password: "",
-    dates: "",
-    sum: "",
- };
+// const initialValues = {
+//     name: "",
+//     username: "",
+//     password: "",
+//     dates: "",
+//     sum: "",
+//  };
 
 export default function Add_subscription() {
 
 
-    const [name, setName] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [dates, setDates] = useState("");
-    const [sum, setSum] = useState("");
+    // const [name, setName] = useState("");
+    // const [username, setUsername] = useState("");
+    // const [password, setPassword] = useState("");
+    // const [dates, setDates] = useState("");
+    // const [sum, setSum] = useState("");
 
     const [list, setList] = useState([]); //list of subscriptions
 
-    const [inputs, setInputs] = useState(initialValues);
+    const [inputs, setInputs] = useState({
+        name: "",
+        username: "",
+        password: "",
+        dates: "",
+        sum: "",
+    });
 
     const handleInputChange = (e) => {
         e.preventDefault();
 
         const { name, value } = e.target;
-        setInputs({
-            ...inputs,
-            [name]: value,
+        setInputs((prev) => {
+            return {...prev, [name]: value}
         });
     };
 
-    const handleAddSub = (e) => {
-        e.preventDefault();
 
-        console.log(inputs)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { name, username, password, dates, sum } = inputs;    //Here should be
+        setList([
+            ...list,
+            {name, username, password, dates, sum }
+        ])
+         console.log(inputs)   //all inputs should render in list
+        setInputs({name:"", username: "", password: "", dates: "", sum: ""})
     }
 
-    //upgrade list of subs?
 
+    // const handleAddSub = (e) => {   upgrade list of subs?
+    //     e.preventDefault();
+    //
+    //     console.log(inputs)
+    // }
 
 
     return(
-            <div className="container">
+        <div className="container">
             <div className="add_sub">
                 <h1>Now, it's time for add some subs </h1>
 
-            <div className="form_content">
+                <div className="form_content">
 
-            <form className="form-box">
-                <input type="text"
-                       name="name"
-                       placeholder="Name of the platform"
-                       value={inputs.name}
-                       onChange={handleInputChange}/>
+                    <form onSubmit={handleSubmit} className="form-box">
+                        <input type="text"
+                               name="name"
+                               placeholder="Name of the platform"
+                               value={inputs.name}
+                               onChange={handleInputChange}/>
 
-                <input type="text"
-                       name="username"
-                       placeholder="Username for the platform"
-                       value={inputs.username}
-                       onChange={handleInputChange}/>
+                        <input type="text"
+                               name="username"
+                               placeholder="Username for the platform"
+                               value={inputs.username}
+                               onChange={handleInputChange}/>
 
-                <input type="text"
-                       name="password"
-                       placeholder="Password for the platform"
-                       value={inputs.password}
-                       onChange={handleInputChange}/>
+                        <input type="text"
+                               name="password"
+                               placeholder="Password for the platform"
+                               value={inputs.password}
+                               onChange={handleInputChange}/>
 
-                <input type="text"
-                       name="dates"
-                       placeholder="When subscription expires?"
-                       value={inputs.dates}
-                       onChange={handleInputChange}/>
+                        <input type="text"
+                               name="dates"
+                               placeholder="When subscription expires?"
+                               value={inputs.dates}
+                               onChange={handleInputChange}/>
 
-                <input type="number"
-                       name="sum"
-                       placeholder="How much do you pay?"
-                       value={inputs.sum}
-                       onChange={handleInputChange}/>
+                        <input type="number"
+                               name="sum"
+                               placeholder="How much do you pay?"
+                               value={inputs.sum}
+                               onChange={handleInputChange}/>
 
-            </form>
+                        <button
+                            type="submit"
+                            className="btn-add-sub"
+                            // onClick={handleAddSub}                //dodaj elem do listy/ tabeli
+                        >+</button>
 
-                <button type="submit"
-                        className="btn-add-sub"
-                         onClick={handleAddSub}                //dodaj elem do listy/ tabeli
-                >+</button>
+                    </form>
+                </div>
             </div>
-         </div>
-                <h1></h1>
-    </div>
+            <div className="your_subs">
+                <h1>Your subscriptions</h1>
+
+            </div>
+        </div>
     )
 }
+
+
+
 
--- src/components/dashboard/Your_subscriptions.jsx
+++ src/components/dashboard/Your_subscriptions.jsx
@@ -1,4 +1,4 @@
-// import from "./Add_subscription.jsx";
+//import list from "./Add_subscription.jsx";
 import React, {useState} from "react";
 
 
@@ -8,11 +8,34 @@ export default function Your_subscriptions() {
 
     return(
         <div className="subscriptions-list">
-            <h1>Your subscriptions</h1>
             <div className="listOfSubs">
-                <ul>
-                    <li></li> //here should be list of subs
-                </ul>
+                {/*<table border={1} width={"30%"} cellPadding={10}>*/}
+                {/*    <tbody>*/}
+                {/*    <tr>*/}
+                {/*        <td>Platform</td>*/}
+                {/*        <td>Username</td>*/}
+                {/*        <td>Password</td>*/}
+                {/*        <td>Dates</td>*/}
+                {/*        <td>Sum</td>*/}
+                {/*    </tr>*/}
+                {/*    {*/}
+                {/*        list.map(*/}
+                {/*            (info, idx) => {*/}
+                {/*                return(*/}
+                {/*                    <tr key={idx}>*/}
+                {/*                        <td>{info.name}</td>*/}
+                {/*                        <td>{info.username}</td>*/}
+                {/*                        <td>{info.password}</td>*/}
+                {/*                        <td>{info.dates}</td>*/}
+                {/*                        <td>{info.sum}</td>*/}
+
+                {/*                    </tr>*/}
+                {/*                )*/}
+                {/*            }*/}
+                {/*        )*/}
+                {/*    }*/}
+                {/*    </tbody>*/}
+                {/*</table>*/}
             </div>
 
        </div>
